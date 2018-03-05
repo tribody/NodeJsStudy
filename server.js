d4cc3e972feb689a863b7fe22c17cab7c31f6bc3
River He <hctongxue@163.com>
2018-03-05 22:39:42 +0800

    I/O block server

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	http.createServer(function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		var content = route(handle, pathname);
+		response.write(content);
+		response.end();
+	}).listen(8888);
+
+	console.log("Server has started.");
+}
+
+exports.start = start;
